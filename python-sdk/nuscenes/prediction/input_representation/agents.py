69fd4c8a563c626c2176c8e16233c70330d8f127
Ke Sun <sunke.polyu@gmail.com>
2021-04-17 17:05:48 -0400

    Draw the ego vehicle
    If required, the faded agent representation will also include the
    ego vehicle. The ego is drawn in cyan just to be distringuishable
    from other yellow and red agents.

--- python-sdk/nuscenes/prediction/input_representation/agents.py
+++ python-sdk/nuscenes/prediction/input_representation/agents.py
@@ -151,6 +151,8 @@ def default_colors(category_name: str) -> Tuple[int, int, int]:
         return 204, 0, 204  # violet
     elif 'human' in category_name or 'animal' in category_name:
         return 255, 153, 51  # orange
+    elif category_name == 'ego':
+        return 0, 255, 255 # cyan
     else:
         raise ValueError(f"Cannot map {category_name} to a color.")
 
@@ -195,6 +197,133 @@ def draw_agent_boxes(center_agent_annotation: Dict[str, Any],
 
             cv2.fillPoly(base_image, pts=[np.int0(box)], color=color)
 
+def get_present_for_ego(helper: PredictHelper, sample_token: str) -> Dict[str, Any]:
+    """
+    Get the present data for the ego vehicle.
+
+    :param: helper The PredictHelper object.
+    :param: sample_token The token of the present sample.
+    :return: The present ego pose data.
+    """
+
+    nusc = helper.data
+
+    sample = nusc.get('sample', sample_token)
+    cam_front_data_token = sample['data']['CAM_FRONT']
+    cam_front_data = nusc.get('sample_data', cam_front_data_token)
+    ego_pose_token = cam_front_data['ego_pose_token']
+
+    return nusc.get('ego_pose', ego_pose_token)
+
+def get_past_for_ego(helper: PredictHelper, sample_token: str, seconds: int) -> List[Dict[str, Any]]:
+    """
+    Get the past data for the ego vehicle.
+
+    :param: helper The PredictHelper object.
+    :param: sample_token The token of the present sample.
+    :param: seconds The duration to look back in the past.
+    :return: A list of past ego pose data.
+             The present ego pose data (associated with the sample_token) is not included in the list.
+             The most distant ego pose in time is at index 0 of the returned list.
+    """
+
+    nusc = helper.data
+
+    sample = nusc.get('sample', sample_token)
+    present_time = sample['timestamp']
+
+    past_ego_pose = []
+
+    while True:
+
+        sample_token = sample['prev']
+
+        # Break if there is no more past data.
+        if not sample_token: break
+
+        sample = nusc.get('sample', sample_token)
+
+        # Break if enough history data has been retrieved.
+        if helper._absolute_time_diff(sample['timestamp'], present_time) > seconds: break
+
+        # Get the ego pose data associated with this sample.
+        cam_front_data_token = sample['data']['CAM_FRONT']
+        cam_front_data = nusc.get('sample_data', cam_front_data_token)
+        ego_pose_token = cam_front_data['ego_pose_token']
+        past_ego_pose.append(nusc.get('ego_pose', ego_pose_token))
+
+    # Reverse the data so that the most distant ego pose in time is at index 0.
+    past_ego_pose.reverse()
+
+    return past_ego_pose
+
+def get_ego_track_box(ego_pose: Dict[str, Any],
+                      center_coordinates: Tuple[float, float],
+                      center_pixels: Tuple[float, float],
+                      resolution: float = 0.1) -> np.ndarray:
+    """
+    Get four corners of bounding box for agent in pixels.
+    :param ego_pose: The ego pose data.
+    :param center_coordinates: (x, y) coordinates in global frame
+        of the center of the image.
+    :param center_pixels: (row_index, column_index) location of the center
+        of the image in pixel coordinates.
+    :param resolution: Resolution pixels/meter of the image.
+    """
+
+    assert resolution > 0
+
+    location = ego_pose['translation'][:2]
+    yaw_in_radians = quaternion_yaw(Quaternion(ego_pose['rotation']))
+
+    row_pixel, column_pixel = convert_to_pixel_coords(location,
+                                                      center_coordinates,
+                                                      center_pixels, resolution)
+
+    width = 1.8 / resolution
+    length = 4.0 / resolution
+
+    # Width and length are switched here so that we can draw them along the x-axis as
+    # opposed to the y. This makes rotation easier.
+    return pixels_to_box_corners(row_pixel, column_pixel, length, width, yaw_in_radians)
+
+def draw_ego_boxes(center_agent_annotation: Dict[str, Any],
+                   center_agent_pixels: Tuple[float, float],
+                   ego_history: List[Dict[str, Any]],
+                   base_image: np.ndarray,
+                   get_color: Callable[[str], Tuple[int, int, int]],
+                   resolution: float = 0.1) -> None:
+    """
+    Draws past sequence of agent boxes on the image.
+    :param center_agent_annotation: Annotation record for the agent
+        that is in the center of the image.
+    :param center_agent_pixels: Pixel location of the agent in the
+        center of the image.
+    :param ego_history: History for the ego in the scene.
+    :param base_image: Image to draw the agents in.
+    :param get_color: Mapping from category_name to RGB tuple.
+    :param resolution: Size of the image in pixels / meter.
+    :return: None.
+    """
+
+    agent_x, agent_y = center_agent_annotation['translation'][:2]
+
+    num_points = len(ego_history)
+
+    for i, ego_pose in enumerate(ego_history):
+
+        box = get_ego_track_box(ego_pose, (agent_x, agent_y), center_agent_pixels, resolution)
+
+        color = get_color('ego')
+
+        # Don't fade the colors if there is no history
+        if num_points > 1:
+            color = fade_color(color, i, num_points - 1)
+
+        cv2.fillPoly(base_image, pts=[np.int0(box)], color=color)
+
+    return
+
 
 class AgentBoxesWithFadedHistory(AgentRepresentation):
     """
@@ -208,7 +337,8 @@ class AgentBoxesWithFadedHistory(AgentRepresentation):
                  resolution: float = 0.1,  # meters / pixel
                  meters_ahead: float = 40, meters_behind: float = 10,
                  meters_left: float = 25, meters_right: float = 25,
-                 color_mapping: Callable[[str], Tuple[int, int, int]] = None):
+                 color_mapping: Callable[[str], Tuple[int, int, int]] = None,
+                 include_ego: bool = True):
 
         self.helper = helper
         self.seconds_of_history = seconds_of_history
@@ -228,6 +358,8 @@ class AgentBoxesWithFadedHistory(AgentRepresentation):
             color_mapping = default_colors
 
         self.color_mapping = color_mapping
+
+        self.include_ego = include_ego
 
     def make_representation(self, instance_token: str, sample_token: str) -> np.ndarray:
         """
@@ -262,6 +394,18 @@ class AgentBoxesWithFadedHistory(AgentRepresentation):
 
         draw_agent_boxes(center_agent_annotation, central_track_pixels,
                          history, base_image, resolution=self.resolution, get_color=self.color_mapping)
+
+        # Draw past of the ego vehicle.
+        if self.include_ego:
+
+            ego_history = get_past_for_ego(self.helper, sample_token, self.seconds_of_history)
+
+            ego_present_time = get_present_for_ego(self.helper, sample_token)
+
+            ego_history.append(ego_present_time)
+
+            draw_ego_boxes(center_agent_annotation, central_track_pixels,
+                           ego_history, base_image, resolution=self.resolution, get_color=self.color_mapping)
 
         center_agent_yaw = quaternion_yaw(Quaternion(center_agent_annotation['rotation']))
         rotation_mat = get_rotation_matrix(base_image.shape, center_agent_yaw)
