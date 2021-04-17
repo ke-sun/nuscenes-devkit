be1bf8d6743cfa540c8e262c9e2ad078913b2707
Ke Sun <sunke.polyu@gmail.com>
2021-04-17 15:33:42 -0400

    Close the files properly

--- python-sdk/nuscenes/eval/prediction/splits.py
+++ python-sdk/nuscenes/eval/prediction/splits.py
@@ -20,17 +20,19 @@ def get_prediction_challenge_split(split: str, dataroot: str = '/data/sets/nusce
     """
     if split not in {'mini_train', 'mini_val', 'train', 'train_val', 'val'}:
         raise ValueError("split must be one of (mini_train, mini_val, train, train_val, val)")
-    
+
     if split == 'train_val':
         split_name = 'train'
     else:
         split_name = split
 
     path_to_file = os.path.join(dataroot, "maps", "prediction", "prediction_scenes.json")
-    prediction_scenes = json.load(open(path_to_file, "r"))
+    scene_file = open(path_to_file, "r")
+    prediction_scenes = json.load(scene_file)
+    scene_file.close()
     scenes = create_splits_scenes()
     scenes_for_split = scenes[split_name]
-    
+
     if split == 'train':
         scenes_for_split = scenes_for_split[NUM_IN_TRAIN_VAL:]
     if split == 'train_val':
